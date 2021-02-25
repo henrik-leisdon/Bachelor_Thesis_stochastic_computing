7944f86ca1ae6bcb16f7968c21169595c5a02525
henrik-leisdon <henrik.leisdon@uni-weimar.de>
2021-02-25 09:33:05 +0100

    simple circuit simulator. proof of concept/how to start

--- sc_sim/main.py
+++ sc_sim/main.py
@@ -1,18 +1,22 @@
+from fractions import Fraction
+import random
+
+# https://www.collindelker.com/2014/08/29/electrical-schematic-drawing-python.html for drawing circuits
+
 # 1. build gates
 
+
 class XOR:
-    def __init__(self, tau):  # tau is the limit for iterations
+    def __init__(self, tau, x, y):  # tau is the limit for iterations
         self.__tau = tau
+        self.__x = x
+        self.__y = y
 
     def get_tau(self):
         return self.__tau
 
-    def set_tau(self, tau):
-        self.__tau = tau
-
-    @staticmethod
-    def l_xor(x, y):  # l_xor for logical xor
-        if x == 1 and y == 1 or x == 0 and y == 0:
+    def l_xor(self):  # l_xor for logical xor
+        if self.__x == 1 and self.__y == 1 or self.__x == 0 and self.__y == 0:
             return 0
         else:
             return 1
@@ -64,11 +68,40 @@ def l_not(x):
 # 1.1 build stochastic main core
 # 1.2 stochastic number generator
 
-class StochasticNumberGenerator:    # in probability y_i
-    def __init__(self, tau):  # tau is the limit for iterations
-        self.__tau = tau
+class InitialProbabilityCalculator:
+    def __int__(self, p_e):
+        self.__p_e = p_e
 
+
+class StochasticNumberGenerator:
+    def __init__(self, tau, input_y, stream_length):  # tau is the limit for iterations
+        self.__tau = tau
+        self.__input_y = input_y
+        self.__bitstream = []
+        self.__stream_length = stream_length
+
+    def generate(self):
+        frac = Fraction(self.__input_y)
+        for i in range(0, self.__stream_length):
+            m = random.randint(0, frac.denominator)
+            if m < frac.numerator:
+                self.__bitstream.append(1)
+            else:
+                self.__bitstream.append(0)
+        return self.__bitstream
+
+
+class WeightedStochasticNumberGenerator:    # in probability y_i
+    def __init__(self, tau, input_y):  # tau is the limit for iterations
+        self.__tau = tau
+        self.__input_y = input_y
+
+    def generate(self):
+        probability = self.__input_y
+        frac = Fraction(probability)
+        list(bin(frac.numerator)[2:])
     # (probability).as_integer_ratio()
+    # randbytes
     # from fractions import Fraction
     # Fraction(probability)
 
@@ -78,12 +111,16 @@ class StochasticNumberGenerator:    # in probability y_i
     # n weight functions
     # w_n-1 = and(not(w_n), l_n-1)
 
-    #
 
+class MainStochasticCore:
+    def __init__(self, tau, bitlist_y):
+        self.__tau = tau
+        self.__bitlist_y = bitlist_y
 
-
-
-
+    def calculate_core(self):
+        xor_1 = XOR(self.__tau, self.__bitlist_y[2], self.__bitlist_y[3])
+        xor_2 = XOR(self.__tau,y_o, y_3)
+        xor_3 = XOR(self.__tau, y_0,y_2)
 # 2. build queue
 
 # 3. hardcode algorithm
