96cdf4d3dea1214ef998b8fc17cb25e3061b5c77
henrik-leisdon <henrik.leisdon@uni-weimar.de>
2021-03-06 14:39:22 +0100

    gates and queue now working, add handler to handle pipeline

--- sc_sim/circuit.py
+++ sc_sim/circuit.py
@@ -2,23 +2,26 @@ import SNG
 import PC
 import STB
 import sc_circuit
+import sc_queue
 
 
 def main():
     input = [1, 0, 0, 1, 1, 1]
 
-    #sng = SNG.StochasticNumberGenerator('sng', 0.1, 10)
-    #y_in = sng.generate_stochastic_bitstream(input)
+    # sng = SNG.StochasticNumberGenerator('sng', 0.1, 10)
+    # y_in = sng.generate_stochastic_bitstream(input)
 
     wsng = SNG.WeightedStochasticNumberGenerator('sng', 0.1)
     y_in = wsng.generate_stochastic_bitstream(input)
+    circuit = sc_queue.Circuit('queue_circuit')
+    circuit.generate()
 
     parity = False
     i = 0
     threshold = 10
     while parity == False and i < threshold:
         print('yin = ' + str(y_in))
-        y_out = sc_circuit.evaluate_msc(y_in)
+        y_out = circuit.run_circuit(y_in)
         x_out = STB.convert_all(y_out)
         parity = PC.parity_check(x_out)
         print(parity)
--- sc_sim/sc_queue.py
+++ sc_sim/sc_queue.py
@@ -4,166 +4,164 @@ class Connection:
         self.value = None
         self.connection = []
 
-    def set_value(self, value):
-        self.value = value
-
     def connect(self, connections):
         if not isinstance(connections, list):
             connections = [connections]
-        for connection in connections:
-            self.connections.append(connection)
+        for con in connections:
+            self.connection.append(con)
+
 
 class Gate:
     def __init__(self, name):
         self.name = name
+
+
+class Gate2(Gate):
+    def __init__(self, name):
+        Gate.__init__(self, name)
         self.value = None
-        self.connections = []
+        self.connection = []
+
+    def update(self):
+        if self.value is not None:
+            for con in self.connection:
+                con.value = self.value
 
     def connect(self, connections):
         if not isinstance(connections, list):
             connections = [connections]
-        for connection in connections:
-            self.connections.append(connection)
-
-    def update(self):
-        if self.value is not None:
-            for connection in self.connections:
-                connection.set_input(self.value)
-
-class Gate2(Gate):
-    def __init__(self,name):
-        Gate.__init__(self,name)
+        for con in connections:
+            self.connection.append(con)
 
 
 class Input(Gate):
     def __init__(self, name):
         Gate.__init__(self, name)
+        self.value = None
+        self.connection = []
 
     def set_input(self, input):
         self.value = input
 
+    def is_evaluatable(self):
+        return True
+
     def evaluate(self):
             pass
+
+    def update(self):
+        if self.value is not None:
+            for con in self.connection:
+                con.value = self.value
+
+    def connect(self, connections):
+        if not isinstance(connections, list):
+            connections = [connections]
+        for con in connections:
+            self.connection.append(con)
 
 
 class Output(Gate):
     def __init__(self, name):
         Gate.__init__(self, name)
-
-    def set_output(self, input):
-        self.value = input
-
-
-class XOR(Gate):
-    def __init__(self, name):
-        Gate.__init__(self, name)
+        self.in_1 = Connection('in')
         self.value = None
-        self.in_1 = Connection('in_1')
-        self.in_2 = Connection('in_2')
-        self.out = Connection('out')
 
     def is_evaluatable(self):
-        if self.input_1 is None or self.input_2 is None:
+        return True
+
+    def evaluate(self):
+        pass
+
+    def update(self):
+        pass
+
+
+class XOR(Gate2):
+    def __init__(self, name):
+        Gate2.__init__(self, name)
+        self.in_1 = Connection('in_1')
+        self.in_2 = Connection('in_2')
+
+    def is_evaluatable(self):
+        if self.in_1.value is None or self.in_2.value is None:
             return False
         else:
             return True
 
     def evaluate(self):
+        print('xor evaluate ' + str(self.in_1.value))
         if self.in_1.value == 1 and self.in_2.value == 0 or self.in_1.value == 0 and self.in_2.value == 1:
             self.value = 1
         else:
             self.value = 0
 
-    def set_input(self, value):
-        if self.input_1 is None:
-            self.input_1 = value
-        elif self.input_2 is None:
-            self.input_2 = value
-        else:
-            print('Error in Gate' + self.name + ', too many inputs assigned')
 
 class AND(Gate):
     def __init__(self, name):
         Gate.__init__(self, name)
         self.value = None
-        self.input_1 = None
-        self.input_2 = None
+        self.in_1 = Connection('in_1')
+        self.in_2 = Connection('in_2')
 
     def is_evaluatable(self):
-        if self.input_1 is None or self.input_2 is None:
+        if self.in_1.value is None or self.in_2.value is None:
             return False
         else:
             return True
 
     def evaluate(self):
-        if self.input_1 == 0 or self.input_2 == 0:
+        if self.in_1.value == 0 or self.in_2.value == 0:
             self.value = 0
-        elif self.input_1 == 1 and self.input_2 == 1:
+        elif self.in_1.value == 1 and self.in_2.value == 1:
             self.value = 1
         else:
             self.value = 0
 
-    def set_input(self, value):
-        if self.input_1 is None:
-            self.input_1 = value
-        elif self.input_2 is None:
-            self.input_2 = value
-        else:
-            print('Error in Gate' + Gate.__name__ + ', too many inputs assigned')
 
-
-class Update(Gate):
+class Update(Gate2):
     def __init__(self, name):
-        Gate.__init__(self, name)
-        self.value = None
-        self.input_1 = None
-        self.input_2 = None
-        self.input_3 = None
+        Gate2.__init__(self, name)
+        self.in_1 = Connection('in_1')
+        self.in_2 = Connection('in_2')
+        self.in_3 = Connection('in_3')
 
     def is_evaluatable(self):
-        if self.input_1 is None or self.input_2 is None or self.input_3 is None:
+        if self.in_1.value is None or self.in_2.value is None or self.in_3.value is None:
             return False
         else:
             return True
 
     def evaluate(self):
-        if self.input_1 == 1 and self.input_2 == 1:
+        if self.in_1.value == 1 and self.in_2.value == 1:
             self.value = 1
-        elif self.input_1 == 0 and self.input_2 == 0:
+        elif self.in_1.value == 0 and self.in_2.value == 0:
             self.value = 0
         else:
-            self.value = self.input_3
+            self.value = self.in_3.value
 
-    def set_input(self, value):
-        if self.input_1 is None:
-            self.input_1 = value
-        elif self.input_2 is None:
-            self.input_2 = value
-        elif self.input_3 is None:
-            self.input_3 = value
-        else:
-            print('Error in Gate' + Gate.__name__ + ', too many inputs assigned')
 
 class Circuit(Gate):
     def __init__(self, name):
         Gate.__init__(self, name)
+        self.gate_list = []
+        self.y_0 = Input('y_0')
+        self.y_1 = Input('y_1')
+        self.y_2 = Input('y_2')
+        self.y_3 = Input('y_3')
+        self.y_4 = Input('y_4')
+        self.y_5 = Input('y_5')
 
+        self.y_0_out = Output('y_0_out')
+        self.y_1_out = Output('y_1_out')
+        self.y_2_out = Output('y_2_out')
+        self.y_3_out = Output('y_3_out')
+        self.y_4_out = Output('y_4_out')
+        self.y_5_out = Output('y_5_out')
 
     def generate(self):
+        print('circuit initialized')
         # set input
-        y_0 = Input('y_0')
-        y_1 = Input('y_1')
-        y_2 = Input('y_2')
-        y_3 = Input('y_3')
-        y_4 = Input('y_4')
-        y_5 = Input('y_5')
-
-        y_0_out = Output('y_0_out')
-        y_1_out = Output('y_1_out')
-        y_2_out = Output('y_2_out')
-        y_3_out = Output('y_3_out')
-        y_4_out = Output('y_4_out')
-        y_5_out = Output('y_5_out')
 
         xor_0 = XOR('XOR_0')
         xor_1 = XOR('XOR_1')
@@ -173,13 +171,58 @@ class Circuit(Gate):
         update_1 = Update('Update_1')
         update_2 = Update('Update_2')
 
-        y_0.connect([xor_1, xor_2, update_0, y_4_out])
-        y_1.connect([update_2, y_5_out,update_1])
-        y_2.connect([xor_0, update_1, xor_2, update_2])
+        self.y_0.connect([xor_1.in_1, xor_2.in_1, update_0.in_3, self.y_4_out])
+        self.y_1.connect([update_2.in_2, self.y_5_out,update_1.in_3])
+        self.y_2.connect([xor_0.in_1, update_1.in_1, xor_2.in_2, update_2.in_3])
+        self.y_3.connect([xor_0.in_2, xor_1.in_2])
+        self.y_4.connect([update_0.in_2])
+        self.y_5.connect([update_1.in_2])
 
+        xor_0.connect([update_0.in_1])
+        xor_1.connect([update_2.in_1])
+        xor_2.connect([self.y_3_out])
 
+        update_0.connect([self.y_0_out])
+        update_1.connect([self.y_1_out])
+        update_2.connect([self.y_2_out])
 
-        self.gate_list.append()
+        self.gate_list.append(self.y_0)
+        self.gate_list.extend([self.y_1, self.y_2, self.y_3, self.y_4, self.y_5, xor_0, xor_1, xor_2, update_0,
+                               update_1, update_2, self.y_0_out, self.y_1_out, self.y_2_out, self.y_3_out,
+                               self.y_4_out, self.y_5_out])
 
     def run_circuit(self, input):
+        print('run circuit')
+        self.y_0.value = input[0]
+        self.y_1.value = input[1]
+        self.y_2.value = input[2]
+        self.y_3.value = input[3]
+        self.y_4.value = input[4]
+        self.y_5.value = input[5]
 
+        while self.gate_list:
+            gate = self.gate_list.pop(0)
+            if gate.is_evaluatable:
+                print('in evaluatable')
+                gate.evaluate()
+                print(gate.name + ' evaluated: ' + str(gate.value))
+                gate.update()
+            else:
+                self.gate_list.append(gate)
+
+        print('end')
+        y_out = []
+        y_out.extend([self.y_0_out.value, self.y_1_out.value, self.y_2_out.value, self.y_3_out.value,
+                      self.y_4_out.value, self.y_5_out.value])
+        return y_out
+
+
+def main():
+    c = Circuit('c')
+    c.generate()
+    y = c.run_circuit([1, 0, 0, 1, 1, 1])
+    print(y)
+
+
+if __name__ == '__main__':
+    main()
