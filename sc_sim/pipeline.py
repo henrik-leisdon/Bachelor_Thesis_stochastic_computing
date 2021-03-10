20f8cc46b374dd98b0a923f9ef7966bc34092b72
henrik-leisdon <henrik.leisdon@uni-weimar.de>
2021-03-10 17:10:55 +0100

    add pipeline class

---
+++ sc_sim/pipeline.py
@@ -0,0 +1,28 @@
+import SNG
+import PC
+import STB
+import sc_circuit
+import sc_queue
+
+
+class Pipeline:
+    def __init__(self):
+        self.sng = SNG.WeightedStochasticNumberGenerator('sng', 0.1)
+        self.msc = sc_queue.MscHandler('circuit')
+        self.stb = STB.StochToBin('stb')
+
+
+        #link
+        self.stb.msc_link = self.msc
+        self.msc.sng_link = self.sng
+        self.msc.stb_link = self.stb
+
+    def pipeline(self, input, bitlength):
+        # request n bits with STB
+        # stb.call(20) 20 bits called
+
+        y_bit = self.stb.request_bits(input, bitlength)
+
+
+
+
